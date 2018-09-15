b58b33de55b613e5c96d23032e04f18fa3b78f6a
Mikayla Robinson <mikayla22robinson@gmail.com>
2018-09-14 23:52:49 -0400

    Separating Files

--- MakingComplementaryDNA.py
+++ MakingComplementaryDNA.py
@@ -1,16 +1,3 @@
-my_DNA = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
-# Find length of the DNA strand
-LengthOfDNA = len(my_DNA)
-# Find how many A's there are
-A_Count = my_DNA.count('A')
-# Find how many T's there are
-T_Count = my_DNA.count('T')
-# Find out how many A's and T's we have together
-CombinedAT = A_Count + T_Count
-# Find the percent!
-ATcontent = CombinedAT / LengthOfDNA * 100
-print(ATcontent)
-
 DNA = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
 # Change A's to filler
 NewDNA1 = DNA.replace('A' , 'Z')
