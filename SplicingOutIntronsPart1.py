926f75d1a7c53ff965c7826ee6051fdca5f31922
Mikayla Robinson <mikayla22robinson@gmail.com>
2018-09-17 11:04:01 -0400

    Refactoring code

--- SplicingOutIntronsPart1.py
+++ SplicingOutIntronsPart1.py
@@ -5,10 +5,10 @@ Splicing out introns to have just the coding regions of the DNA sequence
 GENOMIC_DNA ="ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGA TCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
 
 # Extract the first exon 
-exon_one = GENOMIC_DNA[0:64]
+exon_one = GENOMIC_DNA[0:62]
 
 # Extract the second exon
-exon_two = GENOMIC_DNA[91:1000]
+exon_two = GENOMIC_DNA[90:1000]
 
 #Print the exons combined 
 print(exon_one + exon_two)
