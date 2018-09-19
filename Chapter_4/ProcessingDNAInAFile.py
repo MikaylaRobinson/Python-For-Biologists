c316c2825566849cea757d99abc83bab0ba938cc
Mikayla Robinson <mikayla22robinson@gmail.com>
2018-09-19 11:57:06 -0400

    Processing DNA in a file exercise

---
+++ Chapter_4/ProcessingDNAInAFile.py
@@ -0,0 +1,26 @@
+"""
+Trim each line of DNA in a file and then write the new sequences to a new file and print them.
+"""
+
+# Open the file containing the DNA sequences
+input_file = open("input.txt")
+
+# Create file and open it to write the trimmed DNA sequences
+output_file = open("output.txt", "w")
+
+# Set the it to treat each line of DNA sequences separately 
+for dna in input_file:
+    
+    # Trim each DNA sequence
+    trimmed_dna = dna[14:]
+    
+    # Print each sequence length 
+    print(str(len(trimmed_dna)))
+
+    # Write the trimmed sequences to the output file
+    output_file.write(trimmed_dna)
+
+
+
+
+
