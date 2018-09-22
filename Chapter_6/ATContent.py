16bd1e3c587a794d9950462baa984737b988af5b
Mikayla Robinson <mikayla22robinson@gmail.com>
2018-09-21 20:17:49 -0400

    AT Content and expression level conditional exercise

---
+++ Chapter_6/ATContent.py
@@ -0,0 +1,14 @@
+gene_data = open("data.csv")
+for genes in gene_data:
+    fields = genes.split(",")
+    species_name = fields[0]
+    sequence = fields[1]
+    gene_name = fields[2]
+    expression_level = fields[3]
+    def finding_at_content(dna_sequence):
+        a_count = dna_sequence.upper().count("A")
+        t_count = dna_sequence.upper().count("T")
+        at_content = (a_count + t_count) / len(dna_sequence)
+        return at_content   
+    if finding_at_content(sequence) < 0.5 and int(expression_level) > 200:
+            print(gene_name)
